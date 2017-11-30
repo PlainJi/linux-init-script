8fe70134d79bf6b92272ea37a10a664f65754876
plainji <plain_ji@163.com>
2017-11-30 16:03:55 +0800

    add script

---
+++ install_ros.sh
@@ -0,0 +1,10 @@
+sudo sh -c '. /etc/lsb-release && echo "deb http://mirrors.ustc.edu.cn/ros/ubuntu/ $DISTRIB_CODENAME main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+sudo apt-get update
+sudo apt-get install ros-kinetic-desktop-full
+sudo rosdep init
+rosdep update
+echo "source /opt/ros/kinetic/setup.zsh" >> ~/.zshrc
+source ~/.zshrc
+sudo apt-get install python-rosinstall python-rosinstall-generator python-wstool build-essential
+
