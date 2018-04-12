7c11575a151b7730089bfdcad1a92c116495c1c8
PlainJi <plain_ji@163.com>
2018-04-12 10:45:54 +0800

    add some notes.

--- install_googlepinyin.sh
+++ install_googlepinyin.sh
@@ -3,4 +3,7 @@
 sudo apt-get install -y fcitx fcitx-googlepinyin im-config
 echo "please select <fcitx> in the next window..."
 im-config
-echo "relogin required!!!"
+echo ""
+echo "reboot"
+echo "choose Google Pinyin from Configure Current Input Method."
+echo "config hotkey from Configure."
