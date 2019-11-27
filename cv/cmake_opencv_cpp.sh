15a1937467a76cb6a04faa88506c80f5ce242b88
plainji <plain_ji@163.com>
2019-11-27 21:24:02 +0800

    cv

--- cv/cmake_opencv_cpp.sh
+++ cv/cmake_opencv_cpp.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
+set -x
+
 # Configuration
-HOME_DIR=.
+HOME_DIR=/opt
 VERSION=3.3.1
 
+a() {
 # No GPU
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
       -D CMAKE_INSTALL_PREFIX=/usr/local \
@@ -17,7 +20,8 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE \
 
 # With GPU
 cd ${HOME_DIR}/opencv-${VERSION}/build
-cmake -DCMAKE_CXX_COMPILER=/usr/bin/g++-6 \
+cmake \ 
+      -DCMAKE_CXX_COMPILER=/usr/bin/g++-6 \
       -DCMAKE_C_COMPILER=/usr/bin/gcc-6 \
       -DCUDA_HOST_COMPILER:FILEPATH=/usr/bin/gcc-6 \
       -DCUDA_NVCC_FLAGS='-Xcompiler -D__CORRECT_ISO_CPP11_MATH_H_PROTO --expt-relaxed-constexpr' \
@@ -41,3 +45,27 @@ cmake -DCMAKE_CXX_COMPILER=/usr/bin/g++-6 \
       -DBUILD_PROTOBUF=OFF \
       -DBUILD_opencv_dnn=OFF \
       -DOPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-3.3.1/modules ..
+}
+
+
+cd ${HOME_DIR}/opencv-${VERSION}/build
+cmake \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCATKIN_BUILD_BINARY_PACKAGE=ON \
+      -DCMAKE_INSTALL_PREFIX=${HOME_DIR}/opencv \
+      -DPYTHON_EXECUTABLE=/usr/bin/python3 \
+      -DPYTHON_INCLUDE_DIR=/usr/include/python3.5 \
+      -DPYTHON_LIBRARY=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu/libpython3.5.so \
+      -DPYTHON_BASENAME=-python3.5 \
+      -DSETUPTOOLS_DEB_LAYOUT=OFF \
+      -DENABLE_PRECOMPILED_HEADERS=OFF \
+      -DWITH_TBB=ON \
+      -DWITH_V4L=ON \
+      -DWITH_QT=OFF \
+      -DWITH_OPENGL=ON \
+      -DWITH_CUDA=ON \
+      -DENABLE_FAST_MATH=1 \
+      -DCUDA_FAST_MATH=1 \
+      -DWITH_CUBLAS=1 \
+      -DBUILD_PROTOBUF=OFF \
+      -DBUILD_opencv_dnn=OFF ..
--- cv/make_opencv_cpp.sh
+++ cv/make_opencv_cpp.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
+set -x
+
+# Configuration
+HOME_DIR=/opt
+VERSION=3.3.1
+
 cpu_count=`cat /proc/cpuinfo |grep MHz | wc -l`
 let cpu_use=$cpu_count-1
-echo $cpu_use
+echo "Total CPU: $cpu_use"
 
+cd ${HOME_DIR}/opencv-${VERSION}/build
 make -j$cpu_count
 sudo make install
 sudo ldconfig
